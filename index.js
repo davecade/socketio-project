6e9d73e9cbbd6e901d020636fd4e501dafb43f3f
Dave <dave.cadelina@outlook.com>
2023-05-26 14:28:33 +1000

    custom event socket

--- index.js
+++ index.js
@@ -4,6 +4,21 @@ const http = require("http");
 
 const expressServer = http.createServer(app);
 
+const { Server } = require("socket.io");
+
+const io = new Server(expressServer);
+
+// first param is event, second is callback
+io.on("connection", (socket) => {
+    console.log("New user connected");
+
+    setInterval(() => {
+        let date = new Date();
+        let time = date.getTime();
+        socket.emit("MyEvent", time);
+    }, 1000);
+});
+
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/index.html");
 });
